3f20ff13dd0f97ab19cafd92fce0206d56605b10
Miguel Ramirez <themike123me@gmail.com>
2021-08-15 07:21:32 +0000

    Creation models and migrations

--- employees_menu/models.py
+++ employees_menu/models.py
@@ -1,1 +1,19 @@
+import uuid
 from django.db import models
+
+class Employee(models.Model):
+    name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=100)
+    uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+
+class Menu(models.Model):
+    name = models.CharField(max_length=50)
+    image = models.ImageField(upload_to='uploads/lunch')
+    date = models.DateField()
+    description = models.TextField()
+
+class Order(models.Model):
+    employee = models.ForeignKey(Employee, on_delete=models.CASCADE)
+    menu = models.ForeignKey(Menu, on_delete=models.CASCADE)
+    note = models.TextField()
+    date = models.DateTimeField(editable=False, auto_now_add=True)

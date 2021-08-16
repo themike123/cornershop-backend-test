8af9082f870fc4bab3459cdd7704a23ed8d8c6c4
Miguel Ramirez <themike123me@gmail.com>
2021-08-16 06:56:44 +0000

    Modify Models

--- employees_menu/admin.py
+++ employees_menu/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 
-from .models import Menu
 from .models import Employee
 from .models import Order
+from .models import Lunch
+from .models import Menu
 
-admin.site.register(Menu)
 admin.site.register(Employee)
 admin.site.register(Order)
+admin.site.register(Lunch)
+admin.site.register(Menu)
--- employees_menu/models.py
+++ employees_menu/models.py
@@ -3,17 +3,34 @@ from django.db import models
 
 class Employee(models.Model):
     name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=100)
-    uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    last_name = models.CharField(max_length=100, blank=True) 
 
-class Menu(models.Model):
+class Lunch(models.Model):
     name = models.CharField(max_length=50)
-    image = models.ImageField(upload_to='uploads/lunch')
-    date = models.DateField()
+    photo = models.ImageField(upload_to='uploads/lunch')
     description = models.TextField(blank=True)
 
+    class Meta:
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
+
+class Menu(models.Model):
+    uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    lunchs = models.ManyToManyField(Lunch)
+    date = models.DateField()
+
+    class Meta:
+        ordering = ['date']
+
+    def __str__(self):
+        return self.date
+
 class Order(models.Model):
+    lunch = models.ForeignKey(Lunch, on_delete=models.CASCADE)
     employee = models.ForeignKey(Employee, on_delete=models.CASCADE)
-    menu = models.ForeignKey(Menu, on_delete=models.CASCADE)
     note = models.TextField(blank=True)
     date = models.DateTimeField(editable=False, auto_now_add=True)
+
+#{{ form.employee.as_hidden }}

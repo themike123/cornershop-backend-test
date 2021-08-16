786035cd2fe2120c18cd59d0835669b110a071af
Miguel Ramirez <themike123me@gmail.com>
2021-08-16 05:04:46 +0000

    Config Celery Tasks

---
+++ employees_menu/tasks.py
@@ -0,0 +1,12 @@
+# Create your tasks here
+
+from celery import shared_task
+import requests
+
+SLACK_WEBHOOK = 'https://hooks.slack.com/services/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'
+
+@shared_task
+def send_reminder(message):
+    payload = '{"text":"%s"}' % message
+    response = requests.post(SLACK_WEBHOOK, data=payload) 
+    print(response.text)
